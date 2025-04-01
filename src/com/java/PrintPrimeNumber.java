4eea3fba0b1761da789d0af537b57e43f0710a03
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-01 20:25:09 +0530

    Add more programs

---
+++ src/com/java/PrintPrimeNumber.java
@@ -0,0 +1,22 @@
+package com.java;
+
+public class PrintPrimeNumber {
+    public static void main (String[]args){
+        int size  = 100;
+
+        for(int i=2;i<=size;i++){
+            boolean isPrime = true;
+            for(int j=2;j<i;j++){
+                if(i%j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+
+        }
+
+    }
+}
