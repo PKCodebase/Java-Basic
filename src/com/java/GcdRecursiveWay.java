c81b9c32ce9ce1f5198353af1000881852f1be58
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-22 19:16:00 +0530

    Patterb Programs

---
+++ src/com/java/GcdRecursiveWay.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class GcdRecursiveWay {
+    public static void main(String[]args){
+        int a = 10; int b =20;
+        while(b!=0){
+            int temp = b;
+            b = a%b;
+            a= temp;
+        }
+        System.out.println(a);
+    }
+}
