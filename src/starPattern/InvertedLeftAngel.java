9aa26f194ec4521d2a2f9aa0ca2596cd8a79fe00
PKCodebase <kaushikprasad1659@gmail.com>
2025-04-23 10:22:09 +0530

    Star Pattern

---
+++ src/starPattern/InvertedLeftAngel.java
@@ -0,0 +1,16 @@
+package starPattern;
+
+public class InvertedLeftAngel {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
